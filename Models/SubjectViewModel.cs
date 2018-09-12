2c56acb21039493100440d50dce170e64abd9ebe
UX32L\Андрей <DronLee@list.ru>
2018-09-12 20:56:04 +0300

    Добавлены проверки в модель представления темы.

--- Models/SubjectViewModel.cs
+++ Models/SubjectViewModel.cs
@@ -4,11 +4,10 @@ namespace Retrospective.Models
 {
     public class SubjectViewModel
     {
-        [Required]
+        [Required, StringLength(100)]
         public string Name { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
+        [Required, StringLength(20), DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
