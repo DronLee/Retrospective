f96d0e6e0e4e813b2239a3998447d682bed085dc
UX32L\Андрей <DronLee@list.ru>
2018-09-14 14:29:26 +0300

    Добавлена миграция на создание уникального ключа по полю Name таблицы Subject.

---
+++ Migrations/20180912191316_subject_name_key.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retrospective.Migrations
+{
+    public partial class subject_name_key : Migration
+    {
+        private const string tableName = "Subjects";
+        private const string keyName = "UK_Subject_Name";
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(keyName, tableName, "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(keyName, tableName);
+        }
+    }
+}
