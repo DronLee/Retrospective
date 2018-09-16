a020e846ad021f9f5b8ad5befcde1a3420e86b62
UX32L\Андрей <DronLee@list.ru>
2018-09-17 00:54:46 +0300

    Добавлена миграция на добавление поля Text в таблицу Record, в которое и будет писаться текст записи.

---
+++ Migrations/20180914202707_record_text.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retrospective.Migrations
+{
+    public partial class record_text : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Records",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Records");
+        }
+    }
+}
