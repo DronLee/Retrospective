63410849bd3130dfe159ece5ae9eb245b7bf28d3
UX32L\Андрей <DronLee@list.ru>
2018-09-18 15:23:53 +0300

    Добавил новые свойства в модель представления записей и документировал все свойства. Также убрал лишние using.

--- Models/RecordsViewModel.cs
+++ Models/RecordsViewModel.cs
@@ -1,20 +1,56 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Retrospective.Models
 {
+    /// <summary>
+    /// Модель представления записей со всей информацией, необходимой для отображения.
+    ///</summary>
     public class RecordsViewModel
     {
+        /// <summary>
+        /// Идентификатор темы, к который относятся представленные здесь записи.
+        ///</summary>
         public int SubjectId { get; set; }
         
+        /// <summary>
+        /// Наименование темы, к который относятся представленные здесь записи.
+        ///</summary>
         public string SubjectName { get; set; }
 
+        /// <summary>
+        /// Сами записи.
+        ///</summary>
         public Record[] Records { get; set; }
 
+        /// <summary>
+        /// Выбранный день.
+        ///</summary>
         public string CurrentDay { get; set; }
 
+        /// <summary>
+        /// Список дней, доступных для выбора.
+        ///</summary>
         public List<SelectListItem> Days { get; set; }
+
+        /// <summary>
+        /// Псевдоним создателя новой записи.
+        /// </summary>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// Возможные типы записей.
+        /// </summary>
+        public List<SelectListItem> RecordTypes { get; set; }
+
+        /// <summary>
+        /// Выбранный тип для новой записи.  
+        /// </summary>
+        public byte SelectedRecordType { get; set; }
+
+        /// <summary>
+        /// Текст новой записи.
+        /// </summary>
+        public string NewMessage { get; set; }
     }
 }
