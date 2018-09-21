7e9373a91a315cc200b18720d36fa163b4b125dc
UX32L\Андрей <DronLee@list.ru>
2018-09-21 13:06:00 +0300

    Упростил модель представления темы, так как проверка введёных значений теперь выполнеяется явно.

--- Models/SubjectViewModel.cs
+++ Models/SubjectViewModel.cs
@@ -4,10 +4,9 @@ namespace Retrospective.Models
 {
     public class SubjectViewModel
     {
-        [Required, StringLength(100)]
         public string Name { get; set; }
 
-        [Required, StringLength(20), DataType(DataType.Password)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
