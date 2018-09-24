0884362abeb892270fc6736afef988fcd886fb21
UX32L\Андрей <DronLee@list.ru>
2018-09-24 19:20:08 +0300

    Добавил комментарии в AppDbContext.

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Retrospective.Models
 {
+    /// <summary>
+    /// Класс описывает БД.
+    /// </summary>
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions options)
@@ -9,11 +12,19 @@ namespace Retrospective.Models
         {
         }
 
+        /// <summary>
+        /// Темы.
+        /// </summary>
         public DbSet<Subject> Subjects { get; set; }
+
+        /// <summary>
+        /// Записи.
+        /// </summary>
         public DbSet<Record> Records { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Создание индекса для ускорения поиска.
             modelBuilder.Entity<Record>().HasIndex(r => new { r.SubjectId, r.CreatedOn });
         }
     }
